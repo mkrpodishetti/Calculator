d4c028aa65aedfc6acdc3bde8159efea96251da3
mkrpodishetti <93374783+mkrpodishetti@users.noreply.github.com>
2021-10-29 11:55:26 +0300

    Committed Calculator
    Basic Calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+z=input('enter the operator :')
+x= float(input('please the first number :'))
+y= float(input('please enter the second number :'))
+
+if z=='+':
+    print('Addition of two numbers :',x+y)
+elif z=='-':
+    print('Subraction of two numbers :',x-y)
+elif z=='*':
+    print('Mutliplication of two numbers :',x*y)
+elif z=='/':
+    print('Division of two numbers :',x/y)
+
+
+
+ 
